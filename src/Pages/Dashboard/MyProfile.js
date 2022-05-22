5022bb55ea29b983ea14f1d3f8126475a8c2af7f
ifty03 <ashikulislamb84@gmail.com>
2022-05-22 18:51:51 +0600

    profile updated

--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { MdOutlineBorderColor } from "react-icons/md";
+import SocialIcons from "../../Components/SocialIcons";
 import auth from "../../firebase.init";
 
 const MyProfile = () => {
@@ -9,25 +10,33 @@ const MyProfile = () => {
     <div className="bg-neutral w-11/12 mx-auto mt-8 rounded-lg">
       <div className="flex justify-between items-center pt-5 px-5 ">
         <h1 className="text-3xl font-semibold">My Profile</h1>
-        <h1 className="flex items-center">
+        <button className=" btn btn-primary btn-sm">
           <MdOutlineBorderColor className="mr-2" /> Edit
-        </h1>
+        </button>
       </div>
       <div class="divider"></div>
 
       {/* profile image and information */}
-      <div>
-        <div>
-          <img
-            className="w-48 rounded-t-full"
-            src={user?.photoURL}
-            alt="user profile"
-          />
-          <button className="btn btn-primary">
+      <div className="grid grid-cols-4">
+        <div className="col-span-1 pb-7">
+          <div className="w-40 h-40 mx-auto overflow-hidden rounded-full">
+            <img className="" src={user?.photoURL} alt="user profile" />
+          </div>
+          <button className="btn btn-primary my-6">
             <MdOutlineBorderColor className="mr-2" /> Edit Profile
           </button>
         </div>
-        <div></div>
+        <div className="col-span-3 text-left pl-7">
+          <h2 className="text-md font-semibold text-primary">Full Name:</h2>
+          <p className="text-xl font-semibold mb-2">Your name is here</p>
+          <h2 className="text-md font-semibold text-primary">Email Address:</h2>
+          <p className="text-xl font-semibold mb-2">
+            ashikulislamifty@gmail.com
+          </p>
+          <h2 className="text-md font-semibold text-primary">Role:</h2>
+          <p className="text-xl font-semibold mb-4">Admin</p>
+          <SocialIcons />
+        </div>
       </div>
     </div>
   );
