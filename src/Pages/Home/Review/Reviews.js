805265b8a3b3ea9dd21c1d76949be5acf2efb265
ifty03 <ashikulislamb84@gmail.com>
2022-05-23 09:27:35 +0600

    load daynamic review

--- src/Pages/Home/Review/Reviews.js
+++ src/Pages/Home/Review/Reviews.js
@@ -1,18 +1,25 @@
 import React from "react";
 import Review from "./Review";
 import { BsEmojiSmileFill } from "react-icons/bs";
+import { useQuery } from "react-query";
+import Loading from "../../../Components/Loading/Loading";
 
 const Reviews = () => {
+  const { data, isLoading } = useQuery("review", () =>
+    fetch("http://localhost:5000/reviews").then((res) => res.json())
+  );
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <div>
       <h2 className="text-secondary text-3xl md:text-4xl font-semibold my-16 flex items-center justify-center">
         Our happy client say <BsEmojiSmileFill className="ml-3" />
       </h2>
       <div className=" grid md:grid-cols-2 grid-cols-1 gap-7 w-11/12 mx-auto">
-        <Review />
-        <Review />
-        <Review />
-        <Review />
+        {data?.map((review) => (
+          <Review key={review?._id} review={review}></Review>
+        ))}
       </div>
     </div>
   );
