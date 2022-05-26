c349c754a47355ff8bdefa048e9b0a76a0f33477
ifty03 <ashikulislamb84@gmail.com>
2022-05-26 23:12:04 +0600

    last commit

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,6 +1,6 @@
 import React from "react";
 import bg from "../../Assets/images/bg2.png";
-import bannerImg from "../../Assets/images/banner.png";
+import Fade from "react-reveal/Fade";
 import { Link } from "react-router-dom";
 
 const Banner = () => {
@@ -15,36 +15,38 @@ const Banner = () => {
       <div className="hidden sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-base-100 sm:to-transparent"></div>
 
       <div className="relative max-w-screen-xl px-4 md:py-32 py-10 md:ml-24  lg:h-screen items-center flex flex-wrap-reverse">
-        <div className="max-w-xl text-center sm:text-left">
-          <h1 className="text-3xl font-extrabold sm:text-5xl">
-            Tools is Small
-            <strong className="font-extrabold text-primary sm:block">
-              But Very Usefull
-            </strong>
-          </h1>
+        <Fade left>
+          <div className="max-w-xl text-center sm:text-left">
+            <h1 className="text-3xl font-extrabold sm:text-5xl">
+              Tools is Small
+              <strong className="font-extrabold text-primary sm:block">
+                But Very Usefull
+              </strong>
+            </h1>
 
-          <p className="max-w-lg mt-4 sm:leading-relaxed sm:text-xl">
-            A tool can be any item that is used to achieve a goal. Equipment
-            usually denotes a set of tools that are used to achieve a specific
-            objective
-          </p>
+            <p className="max-w-lg mt-4 sm:leading-relaxed sm:text-xl">
+              A tool can be any item that is used to achieve a goal. Equipment
+              usually denotes a set of tools that are used to achieve a specific
+              objective
+            </p>
 
-          <div className="flex flex-wrap gap-4 mt-8 text-center">
-            <Link
-              className="block w-full px-12 py-3 text-sm font-medium text-white rounded shadow bg-primary sm:w-auto active:bg-primary hover:bg-primary focus:outline-none focus:ring"
-              to="/dashboard"
-            >
-              Get Started
-            </Link>
+            <div className="flex flex-wrap gap-4 mt-8 text-center">
+              <Link
+                className="block w-full px-12 py-3 text-sm font-medium text-white rounded shadow bg-primary sm:w-auto active:bg-primary hover:bg-primary focus:outline-none focus:ring"
+                to="/dashboard"
+              >
+                Get Started
+              </Link>
 
-            <Link
-              className="block w-full px-12 py-3 text-sm font-medium text-black bg-white rounded shadow sm:w-auto hover:text-primary active:text-primary focus:outline-none focus:ring"
-              to="/portfolio"
-            >
-              Learn More
-            </Link>
+              <Link
+                className="block w-full px-12 py-3 text-sm font-medium text-black bg-white rounded shadow sm:w-auto hover:text-primary active:text-primary focus:outline-none focus:ring"
+                to="/portfolio"
+              >
+                Learn More
+              </Link>
+            </div>
           </div>
-        </div>
+        </Fade>
       </div>
     </section>
   );
--- src/Pages/Home/Tools/Tool.js
+++ src/Pages/Home/Tools/Tool.js
@@ -1,60 +1,63 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Fade from "react-reveal/Fade";
 
 const Tool = ({ tool }) => {
   const { name, maximum, minimum, price, available, description, img, _id } =
     tool;
   const navigate = useNavigate();
   return (
-    <div className="card max-w-96 bg-neutral shadow-xl">
-      <figure>
-        <img src={img} alt="Shoes" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">
-          {name}
-          <div className="badge badge-secondary">Sold</div>
-        </h2>
-        <div className="flex flex-start mt-2 items-center">
-          <div className="badge badge-xs badge-accent mr-2"></div>
-          <span className="text-lg font-semibold mr-2">Available: </span>
-          {available}
-        </div>
-        <div className="flex flex-start -mt-2 items-center">
-          <div className="badge badge-xs badge-accent mr-2"></div>
-          <span className="text-lg font-semibold mr-2">Minimum: </span>
-          {minimum}
-        </div>
-        <div className="flex flex-start -mt-2 items-center">
-          <div className="badge badge-xs badge-accent mr-2"></div>
-          <span className="text-lg font-semibold mr-2">Maximum: </span>
-          {maximum}
-        </div>
-        <div className="flex flex-start -mt-2 items-center">
-          <div className="badge badge-xs badge-accent mr-2"></div>
-          <span className="text-lg font-semibold mr-2">Price: </span>
-          {price}
-        </div>
-        <div className="flex flex-start -mt-2 items-center">
-          <div className="badge badge-xs badge-accent mr-2"></div>
-          <span className="text-lg font-semibold mr-2">Description: </span>
-        </div>
-        <p title={description} className="text-left">
-          {description.slice(0, 70)}
-        </p>
+    <Fade left>
+      <div className="card max-w-96 bg-neutral shadow-xl">
+        <figure>
+          <img src={img} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            {name}
+            <div className="badge badge-secondary">Sold</div>
+          </h2>
+          <div className="flex flex-start mt-2 items-center">
+            <div className="badge badge-xs badge-accent mr-2"></div>
+            <span className="text-lg font-semibold mr-2">Available: </span>
+            {available}
+          </div>
+          <div className="flex flex-start -mt-2 items-center">
+            <div className="badge badge-xs badge-accent mr-2"></div>
+            <span className="text-lg font-semibold mr-2">Minimum: </span>
+            {minimum}
+          </div>
+          <div className="flex flex-start -mt-2 items-center">
+            <div className="badge badge-xs badge-accent mr-2"></div>
+            <span className="text-lg font-semibold mr-2">Maximum: </span>
+            {maximum}
+          </div>
+          <div className="flex flex-start -mt-2 items-center">
+            <div className="badge badge-xs badge-accent mr-2"></div>
+            <span className="text-lg font-semibold mr-2">Price: </span>
+            {price}
+          </div>
+          <div className="flex flex-start -mt-2 items-center">
+            <div className="badge badge-xs badge-accent mr-2"></div>
+            <span className="text-lg font-semibold mr-2">Description: </span>
+          </div>
+          <p title={description} className="text-left">
+            {description.slice(0, 70)}
+          </p>
 
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">Limited</div>
-          <div className="badge badge-outline">Effective</div>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Limited</div>
+            <div className="badge badge-outline">Effective</div>
+          </div>
+          <button
+            onClick={() => navigate(`/payment/${_id}`)}
+            className="btn btn-primary w-fit ml-auto mt-5"
+          >
+            Order Now
+          </button>
         </div>
-        <button
-          onClick={() => navigate(`/payment/${_id}`)}
-          className="btn btn-primary w-fit ml-auto mt-5"
-        >
-          Order Now
-        </button>
       </div>
-    </div>
+    </Fade>
   );
 };
 
